3d274e9fa1fedada9794d1fe6c06eab1dcb92b1a
polston <polston@gmail.com>
2016-04-10 22:42:50 +0000

    CommandHandler parses command strings from chat into usable key-value store.

--- CommandHandler.js
+++ CommandHandler.js
@@ -10,12 +10,43 @@ export class CommandHandler
         }
         
         var commandString = event.message.content;
+        var commandParams = this.parse(commandString);
         
-        if(commandString === "poopy") {
-            event.message.channel.sendMessage("buttholes");
-        } else if(commandString === "buttholes") {
-            event.message.channel.sendMessage("some other junk");
+        
+    }
+    
+    parse(commandString) {
+        /**
+         * commandString:
+         * say "hello world" channel general server "Ladies Only"
+         * 
+         * [0]: say, [1]: "hello world", 
+         * [2]: channel, [3]: general, [4]: server, [5]: "Ladies Only"
+         * 
+         * params = {
+              name: "say",
+              param: "hello world",
+              args: {
+                  channel: "general",
+                  server: "Ladies Only"
+              }
+           }
+         */
+        var parts = commandString.match(/(?:[^\s"]+|"[^"]*")+/g);
+        
+        var params = {
+            name: parts[0].replace(new RegExp('"', "g"), ""),
+            param: parts[1].replace(new RegExp('"', "g"), ""),
+            args: {}  // {channel: "general", server: "Ladies Only"}
+        };
+        
+        for(var i = 2; i < parts.length; i += 2) {
+            if (parts.length > i + 1) {
+                params.args[parts[i]] = parts[i + 1].replace(new RegExp('"', "g"), "");
+            }
         }
+        
+        return params;
     }
     
     isSelf(event) {
