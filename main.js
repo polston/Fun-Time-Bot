8541c08f7755c75344bf3a37628f794f80a34ed2
polston <polston@gmail.com>
2016-04-10 21:56:53 +0000

    Abstracted away bot logic from main.js to Bot.js class.

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-// Node Imports
-var Discordie = require("discordie");
-var Events = Discordie.Events;
-
 // JS ES6 Imports
-import { config } from "./config/config"
+import { config } from "./config/config";
+import { Bot } from "./Bot";
 
-// SOME RANDOM JUNK
-
-var discordie = new Discordie();
-discordie.connect(config.bot);
-
-
-var onReady = function(event) {
-    console.log("Connected!");
-};
-
-discordie.Dispatcher.on("GATEWAY_READY", function(event) {
-    console.log("Connected!");
-});
-
-// Events.MESSAGE_CREATE = "MESSAGE_CREATE";
-
-discordie.Dispatcher.on(Events.MESSAGE_CREATE, function(event) {
-    console.log(event.message.content);
-});
+var bot = new Bot(config.bot);
+bot.start();
